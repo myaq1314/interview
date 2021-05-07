9e3ba0a156281a3a831c01535ad32d4c92fa11a3
褚志浩 <916419307@qq.com>
2021-05-07 15:37:58 +0800

    装饰器模式

--- design-mode-interview/src/main/java/org/czh/interview/design_mode_interview/decorator_pattern/DecoratorPatternDemo.java
+++ design-mode-interview/src/main/java/org/czh/interview/design_mode_interview/decorator_pattern/DecoratorPatternDemo.java
@@ -30,6 +30,7 @@ public class DecoratorPatternDemo {
             public ConcreteComponent() {
                 System.out.println("创建具体构件角色");
             }
+
             public void operation() {
                 System.out.println("调用具体构件角色的方法operation()");
             }
