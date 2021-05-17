0674482de4321811b732dd628a515be57f6b4d1d
褚志浩 <916419307@qq.com>
2021-05-17 15:28:52 +0800

    添加amuse模块

---
+++ amuse/src/test/java/org/czh/interview/active_mq_interview/AmuseInterviewTest.java
@@ -0,0 +1,17 @@
+package org.czh.interview.active_mq_interview;
+
+import org.junit.Test;
+
+/**
+ * @author : czh
+ * description :
+ * date : 2021-04-27
+ * email 916419307@qq.com
+ */
+public class AmuseInterviewTest {
+
+    @Test
+    public void test() {
+        System.out.println("AmuseInterviewTest");
+    }
+}
